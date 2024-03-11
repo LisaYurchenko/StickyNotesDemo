1871286cdf3e5acc1150b7031ce0f526d4408036
Lisa Yurchenko <Lisa.Yurchenko@litera.com>
2024-03-11 15:18:38 +0200

    added some design

--- NotesForm.Designer.cs
+++ NotesForm.Designer.cs
@@ -28,43 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotesForm));
             notesListView = new ListView();
-            buttonAdd = new Button();
+            pictureBoxAdd = new PictureBox();
+            pictureBoxMore = new PictureBox();
+            pictureBoxClose = new PictureBox();
+            tableLayoutPanel = new TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxAdd).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxMore).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxClose).BeginInit();
+            tableLayoutPanel.SuspendLayout();
             SuspendLayout();
             // 
             // notesListView
             // 
-            notesListView.Location = new Point(-3, 36);
+            tableLayoutPanel.SetColumnSpan(notesListView, 3);
+            notesListView.Dock = DockStyle.Fill;
+            notesListView.Location = new Point(0, 44);
+            notesListView.Margin = new Padding(0);
             notesListView.Name = "notesListView";
-            notesListView.Size = new Size(332, 325);
+            notesListView.Size = new Size(560, 397);
             notesListView.TabIndex = 0;
             notesListView.UseCompatibleStateImageBehavior = false;
             // 
-            // buttonAdd
+            // pictureBoxAdd
             // 
-            buttonAdd.Location = new Point(-3, 7);
-            buttonAdd.Name = "buttonAdd";
-            buttonAdd.Size = new Size(41, 23);
-            buttonAdd.TabIndex = 1;
-            buttonAdd.Text = "+";
-            buttonAdd.UseVisualStyleBackColor = true;
-            buttonAdd.Click += buttonAdd_Click;
+            pictureBoxAdd.BackColor = Color.Transparent;
+            pictureBoxAdd.Image = (Image)resources.GetObject("pictureBoxAdd.Image");
+            pictureBoxAdd.InitialImage = (Image)resources.GetObject("pictureBoxAdd.InitialImage");
+            pictureBoxAdd.Location = new Point(3, 3);
+            pictureBoxAdd.Name = "pictureBoxAdd";
+            pictureBoxAdd.Size = new Size(38, 38);
+            pictureBoxAdd.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxAdd.TabIndex = 4;
+            pictureBoxAdd.TabStop = false;
+            pictureBoxAdd.Click += pictureBoxAdd_Click;
+            pictureBoxAdd.MouseEnter += pictureBoxAdd_MouseEnter;
+            pictureBoxAdd.MouseLeave += pictureBoxAdd_MouseLeave;
+            // 
+            // pictureBoxMore
+            // 
+            pictureBoxMore.Image = (Image)resources.GetObject("pictureBoxMore.Image");
+            pictureBoxMore.Location = new Point(475, 3);
+            pictureBoxMore.Name = "pictureBoxMore";
+            pictureBoxMore.Size = new Size(38, 38);
+            pictureBoxMore.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxMore.TabIndex = 5;
+            pictureBoxMore.TabStop = false;
+            pictureBoxMore.Click += pictureBoxMore_Click;
+            // 
+            // pictureBoxClose
+            // 
+            pictureBoxClose.Image = (Image)resources.GetObject("pictureBoxClose.Image");
+            pictureBoxClose.Location = new Point(519, 3);
+            pictureBoxClose.Name = "pictureBoxClose";
+            pictureBoxClose.Size = new Size(38, 38);
+            pictureBoxClose.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxClose.TabIndex = 6;
+            pictureBoxClose.TabStop = false;
+            pictureBoxClose.Click += pictureBoxClose_Click;
+            // 
+            // tableLayoutPanel
+            // 
+            tableLayoutPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel.ColumnCount = 3;
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel.Controls.Add(notesListView, 0, 1);
+            tableLayoutPanel.Controls.Add(pictureBoxClose, 2, 0);
+            tableLayoutPanel.Controls.Add(pictureBoxAdd, 0, 0);
+            tableLayoutPanel.Controls.Add(pictureBoxMore, 1, 0);
+            tableLayoutPanel.Location = new Point(0, 0);
+            tableLayoutPanel.Margin = new Padding(0, 0, 0, 10);
+            tableLayoutPanel.Name = "tableLayoutPanel";
+            tableLayoutPanel.RowCount = 2;
+            tableLayoutPanel.RowStyles.Add(new RowStyle());
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel.Size = new Size(560, 441);
+            tableLayoutPanel.TabIndex = 7;
             // 
             // NotesForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(328, 359);
-            Controls.Add(buttonAdd);
-            Controls.Add(notesListView);
+            ClientSize = new Size(559, 460);
+            Controls.Add(tableLayoutPanel);
             Name = "NotesForm";
             Text = "NotesForm";
+            MouseDown += NotesForm_MouseDown;
+            MouseMove += NotesForm_MouseMove;
+            MouseUp += NotesForm_MouseUp;
+            ((System.ComponentModel.ISupportInitialize)pictureBoxAdd).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxMore).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxClose).EndInit();
+            tableLayoutPanel.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private ListView notesListView;
-        private Button buttonAdd;
+        private PictureBox pictureBoxAdd;
+        private PictureBox pictureBoxMore;
+        private PictureBox pictureBoxClose;
+        private TableLayoutPanel tableLayoutPanel;
     }
 }
--- NotesForm.cs
+++ NotesForm.cs
@@ -2,11 +2,39 @@
 {
     public partial class NotesForm : Form
     {
+        private Point mouseOffset;
+        private bool isMouseDown = false;
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            ControlPaint.DrawSizeGrip(e.Graphics, this.BackColor, this.ClientSize.Width - 16, this.ClientSize.Height - 16, 16, 16);
+        }
+
         public NotesForm()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.BackColor = Color.LightYellow;
+            this.ResizeRedraw = true;
         }
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_NCHITTEST = 0x84;
+            const int HTBOTTOMRIGHT = 17;
 
+            if (m.Msg == WM_NCHITTEST)
+            {
+                Point pos = new Point(m.LParam.ToInt32() & 0xffff, m.LParam.ToInt32() >> 16);
+                pos = this.PointToClient(pos);
+                if (pos.X >= this.ClientSize.Width - 16 && pos.Y >= this.ClientSize.Height - 16)
+                {
+                    m.Result = (IntPtr)HTBOTTOMRIGHT;
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }
         private void buttonAdd_Click(object sender, EventArgs e)
         {
             NoteDetailsForm noteDetailsForm = new NoteDetailsForm();
@@ -21,6 +49,85 @@
             notesListView.Columns.Add("colDateTime");
             var noteListItem = new ListViewItem(new string[] { title, creationDate });
             notesListView.Items.Add(noteListItem);
+        }
+
+        private void NotesForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            int xOffset;
+            int yOffset;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                xOffset = -e.X - SystemInformation.FrameBorderSize.Width;
+                yOffset = -e.Y - SystemInformation.CaptionHeight -
+                SystemInformation.FrameBorderSize.Height;
+                mouseOffset = new Point(xOffset, yOffset);
+                isMouseDown = true;
+            }
+        }
+
+        private void NotesForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(mouseOffset.X, mouseOffset.Y);
+                Location = mousePos;
+            }
+        }
+
+        private void NotesForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDown = false;
+            }
+        }
+
+        private void pictureBoxAdd_Click(object sender, EventArgs e)
+        {
+            NoteDetailsForm noteDetailsForm = new NoteDetailsForm();
+            noteDetailsForm.ShowDialog();
+
+            var title = noteDetailsForm.Title;
+            var content = noteDetailsForm.Content;
+            var creationDate = noteDetailsForm.CreationDate.ToString();
+
+            notesListView.View = View.Details;
+            notesListView.Columns.Add("colTitle");
+            notesListView.Columns.Add("colDateTime");
+            var noteListItem = new ListViewItem(new string[] { title, creationDate });
+            notesListView.Items.Add(noteListItem);
+        }
+
+        private void pictureBoxAdd_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBoxAdd.BackColor = Color.LightGray;
+        }
+
+        private void pictureBoxAdd_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBoxAdd.BackColor = Color.Transparent;
+        }
+
+        private void pictureBoxMore_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBoxClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= 0x20000;
+                return cp;
+            }
         }
     }
 }
