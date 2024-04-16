666aa0fec4f960df3c41b87ca5c0a20552121d26
Lisa Yurchenko <Lisa.Yurchenko@litera.com>
2024-04-16 13:21:25 +0300

    unit testing framework examples

--- Models/Note.cs
+++ Models/Note.cs
@@ -20,12 +20,12 @@
         public string Title
         {
             get; set;
-        }
+        } = string.Empty;
 
         public string Content
         {
             get; set;
-        }
+        } = string.Empty;
 
         public DateTime CreationDate
         {
